13b612eab983c7bc05ea3e541e8e5e28e4670301
Rick Cogley <rick.cogley@esolia.co.jp>
2015-05-29 18:32:41 +0900

    Add functions for hugo deploy
    esolia.com and esolia.co.jp hugo deploy functions in zshell.
    
    Changed name to:
    
    hugodeploy-aaaaaa
    
    The hyphen is ok apparently.

--- plugins/rickcogley/rickcogley.plugin.zsh
+++ plugins/rickcogley/rickcogley.plugin.zsh
@@ -2,11 +2,25 @@ export GOPATH=/Users/rcogley/gocode
 export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
 
 # function to deploy hugo, had --force and --progress on rsync, maybe Apple's is old?
-function hugorccdeploy {
+function hugodeploy-rcc {
     rm -rf /tmp/rick.cogley.info
     chmod -R 775 /Users/rcogley/Repositories/RCC-Hugo2015/static/
     hugo -s /Users/rcogley/Repositories/RCC-Hugo2015/ -d /tmp/rick.cogley.info
     rsync -avze "ssh -p 22" --delete /tmp/rick.cogley.info/ rcogley@cogley.info:/home/rcogley/webapps/rick_hugo01
+}
+
+function hugodeploy-esoliacom {
+    rm -rf /tmp/esolia.com
+    chmod -R 775 /Users/rcogley/Repositories/eSolia/static/
+    hugo --config="/Users/rcogley/Repositories/eSolia/config_en.toml" -s /Users/rcogley/Repositories/eSolia/ -d /tmp/esolia.com
+    rsync -avze "ssh -p 22" --delete /tmp/esolia.com/ rcogley@cogley.info:/home/rcogley/webapps/es_hugo_esolia_com_01
+}
+
+function hugodeploy-esoliacojp {
+    rm -rf /tmp/esolia.co.jp
+    chmod -R 775 /Users/rcogley/Repositories/eSolia/static/
+    hugo --config="/Users/rcogley/Repositories/eSolia/config_ja.toml" -s /Users/rcogley/Repositories/eSolia/ -d /tmp/esolia.co.jp
+    rsync -avze "ssh -p 22" --delete /tmp/esolia.co.jp/ rcogley@cogley.info:/home/rcogley/webapps/es_hugo_esolia_co_jp_01
 }
 
 function hugoserveresoliaen {
