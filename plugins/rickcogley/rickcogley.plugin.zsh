c8a180519b3eaa0b927e964c16d6c5a9da6878e3
Rick Cogley <rcogley@RickMBP.esolia.net>
2015-06-12 12:03:24 +0900

    Call homebrew token out of local file
    Github rightly chastised me for committing a token. Duh. Sorry.

--- plugins/rickcogley/rickcogley.plugin.zsh
+++ plugins/rickcogley/rickcogley.plugin.zsh
@@ -1,6 +1,6 @@
 export GOPATH=/Users/rcogley/gocode
 export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
-export HOMEBREW_GITHUB_API_TOKEN="28d34fd1c2f43a62de60c5feb4c156f5979551ed"
+export HOMEBREW_GITHUB_API_TOKEN="$(cat ~/.homebrew_github_api_token)"
 
 
 # function to deploy hugo, had --force and --progress on rsync, maybe Apple's is old?
