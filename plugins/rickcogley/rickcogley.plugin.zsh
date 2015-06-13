c1822f0e30bb252fb2076b81e78795dcb6e36bab
Rick Cogley <rick.cogley@esolia.co.jp>
2015-06-13 20:02:30 +0900

    Update paths to ~/dev (really need to variablize)

--- plugins/rickcogley/rickcogley.plugin.zsh
+++ plugins/rickcogley/rickcogley.plugin.zsh
@@ -5,47 +5,47 @@ export HOMEBREW_GITHUB_API_TOKEN="$(cat ~/.homebrew_github_api_token)"
 
 # function to deploy hugo, had --force and --progress on rsync, maybe Apple's is old?
 function hugodeploy-rcc {
-    cd /Users/rcogley/Repositories/RCC-Hugo2015
+    cd /Users/rcogley/dev/RCC-Hugo2015
     rm -rf /tmp/rick.cogley.info
-    export RCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/RCC-Hugo2015/static/css/rickcogleyinfo.css)"
-    export TOPICSRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/RCC-Hugo2015/static/css/topics.min.css)"
-    export PRISMRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/RCC-Hugo2015/static/css/prism.min.css)"
-    export LIGHTRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/RCC-Hugo2015/static/css/lightbox.min.css)"
-    chmod -R 775 /Users/rcogley/Repositories/RCC-Hugo2015/static/
-    hugo --config="/Users/rcogley/Repositories/RCC-Hugo2015/config.toml" -s /Users/rcogley/Repositories/RCC-Hugo2015/ -d /tmp/rick.cogley.info
+    export RCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/RCC-Hugo2015/static/css/rickcogleyinfo.css)"
+    export TOPICSRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/RCC-Hugo2015/static/css/topics.min.css)"
+    export PRISMRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/RCC-Hugo2015/static/css/prism.min.css)"
+    export LIGHTRCCCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/RCC-Hugo2015/static/css/lightbox.min.css)"
+    chmod -R 775 /Users/rcogley/dev/RCC-Hugo2015/static/
+    hugo --config="/Users/rcogley/dev/RCC-Hugo2015/config.toml" -s /Users/rcogley/dev/RCC-Hugo2015/ -d /tmp/rick.cogley.info
     rsync -avze "ssh -p 22" --delete /tmp/rick.cogley.info/ rcogley@cogley.info:/home/rcogley/webapps/rick_hugo01
 }
 
 function hugodeploy-esoliacom {
-    cd /Users/rcogley/Repositories/eSolia
+    cd /Users/rcogley/dev/eSolia
     rm -rf /tmp/esolia.com
-    export STYLECSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/style.css)"
-    export FEEDEKCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/FeedEk-2.0.min.css)"
-    export FONTELLOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/fontello.min.css)"
-    export GHPMATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/ghpages-materialize.min.css)"
-    export MATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/materialize.min.css)"
-    export PRISMCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/prism.min.css)"
-    export TYPOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/typography.min.css)"
+    export STYLECSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/style.css)"
+    export FEEDEKCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/FeedEk-2.0.min.css)"
+    export FONTELLOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/fontello.min.css)"
+    export GHPMATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/ghpages-materialize.min.css)"
+    export MATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/materialize.min.css)"
+    export PRISMCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/prism.min.css)"
+    export TYPOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/typography.min.css)"
     export LATESTSHA="$(git rev-parse master)"
-    chmod -R 775 /Users/rcogley/Repositories/eSolia/static/
-    hugo --config="/Users/rcogley/Repositories/eSolia/config_en.toml" -s /Users/rcogley/Repositories/eSolia/ -d /tmp/esolia.com
+    chmod -R 775 /Users/rcogley/dev/eSolia/static/
+    hugo --config="/Users/rcogley/dev/eSolia/config_en.toml" -s /Users/rcogley/dev/eSolia/ -d /tmp/esolia.com
     rsync -avze "ssh -p 22" --delete /tmp/esolia.com/ rcogley@cogley.info:/home/rcogley/webapps/es_hugo_esolia_com_01
     curl -X POST "http://util-02.esolia.com/flowdock/v2/flowdock.php?action=chat&chat_name=Auto-Script&chat_content=%40team%20Copied+files+to+ESOLIA.COM+site+via+rsync+including+commit+https%3A%2F%2Fgithub.com%2FeSolia%2FeSolia%2Fcommit%2F$LATESTSHA&chat_tags=esolia.com&flowdock_api=73d6f7%3Df83ad24ab6628fda5ca2b18fff349a5877a928O7o799"
 }
 
 function hugodeploy-esoliacojp {
-    cd /Users/rcogley/Repositories/eSolia
+    cd /Users/rcogley/dev/eSolia
     rm -rf /tmp/esolia.co.jp
-    export STYLECSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/style.css)"
-    export FEEDEKCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/FeedEk-2.0.min.css)"
-    export FONTELLOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/fontello.min.css)"
-    export GHPMATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/ghpages-materialize.min.css)"
-    export MATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/materialize.min.css)"
-    export PRISMCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/prism.min.css)"
-    export TYPOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/Repositories/eSolia/static/css/typography.min.css)"
+    export STYLECSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/style.css)"
+    export FEEDEKCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/FeedEk-2.0.min.css)"
+    export FONTELLOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/fontello.min.css)"
+    export GHPMATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/ghpages-materialize.min.css)"
+    export MATCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/materialize.min.css)"
+    export PRISMCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/prism.min.css)"
+    export TYPOCSS_HASH="$(git log -1 --format='%h' /Users/rcogley/dev/eSolia/static/css/typography.min.css)"
     export LATESTSHA="$(git rev-parse master)"
-    chmod -R 775 /Users/rcogley/Repositories/eSolia/static/
-    hugo --config="/Users/rcogley/Repositories/eSolia/config_ja.toml" -s /Users/rcogley/Repositories/eSolia/ -d /tmp/esolia.co.jp
+    chmod -R 775 /Users/rcogley/dev/eSolia/static/
+    hugo --config="/Users/rcogley/dev/eSolia/config_ja.toml" -s /Users/rcogley/dev/eSolia/ -d /tmp/esolia.co.jp
     rsync -avze "ssh -p 22" --delete /tmp/esolia.co.jp/ rcogley@cogley.info:/home/rcogley/webapps/es_hugo_esolia_co_jp_01
     curl -X POST "http://util-02.esolia.com/flowdock/v2/flowdock.php?action=chat&chat_name=Auto-Script&chat_content=%40team%20Copied+files+to+ESOLIA.CO.JP+site+via+rsync+including+commit+https%3A%2F%2Fgithub.com%2FeSolia%2FeSolia%2Fcommit%2F$LATESTSHA&chat_tags=esolia.co.jp&flowdock_api=73d6f7%3Df83ad24ab6628fda5ca2b18fff349a5877a928O7o799"
 }
@@ -53,35 +53,35 @@ function hugodeploy-esoliacojp {
 # function to run hugo server
 
 function hugoserver-esoliacom {
-    cd /Users/rcogley/Repositories/eSolia
-    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/Repositories/eSolia" --config="/Users/rcogley/Repositories/eSolia/config_en.toml" --port=1377
+    cd /Users/rcogley/dev/eSolia
+    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/dev/eSolia" --config="/Users/rcogley/dev/eSolia/config_en.toml" --port=1377
 }
 
 function hugoserver-esoliacojp {
-    cd /Users/rcogley/Repositories/eSolia
-    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/Repositories/eSolia" --config="/Users/rcogley/Repositories/eSolia/config_ja.toml" --port=1399
+    cd /Users/rcogley/dev/eSolia
+    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/dev/eSolia" --config="/Users/rcogley/dev/eSolia/config_ja.toml" --port=1399
 }
 
 function hugoserver-rcc {
-    cd /Users/rcogley/Repositories/RCC-Hugo2015
-    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/Repositories/RCC-Hugo2015" --config="/Users/rcogley/Repositories/RCC-Hugo2015/config.toml" --port=1313
+    cd /Users/rcogley/dev/RCC-Hugo2015
+    hugo server --buildDrafts --watch --verbose --editor="atom" --source="/Users/rcogley/dev/RCC-Hugo2015" --config="/Users/rcogley/dev/RCC-Hugo2015/config.toml" --port=1313
 }
 
 # function to output hugo config
 
 function hugoconfig-esoliacom {
-	cd /Users/rcogley/Repositories/eSolia
-	hugo config --config="/Users/rcogley/Repositories/eSolia/config_en.toml" > /Users/rcogley/Repositories/eSolia/config_en.txt
+	cd /Users/rcogley/dev/eSolia
+	hugo config --config="/Users/rcogley/dev/eSolia/config_en.toml" > /Users/rcogley/dev/eSolia/config_en.txt
 }
 
 function hugoconfig-esoliacojp {
-	cd /Users/rcogley/Repositories/eSolia
-	hugo config --config="/Users/rcogley/Repositories/eSolia/config_ja.toml" > /Users/rcogley/Repositories/eSolia/config_ja.txt
+	cd /Users/rcogley/dev/eSolia
+	hugo config --config="/Users/rcogley/dev/eSolia/config_ja.toml" > /Users/rcogley/dev/eSolia/config_ja.txt
 }
 
 function hugoconfig-rcc {
-	cd /Users/rcogley/Repositories/RCC-Hugo2015
-	hugo config --config="/Users/rcogley/Repositories/RCC-Hugo2015/config.toml" > /Users/rcogley/Repositories/RCC-Hugo2015/config.txt
+	cd /Users/rcogley/dev/RCC-Hugo2015
+	hugo config --config="/Users/rcogley/dev/RCC-Hugo2015/config.toml" > /Users/rcogley/dev/RCC-Hugo2015/config.txt
 }
 
 function hugowatchesolias {
